eac6ffd8dc3e95e5caea2ca4835ecf96c87516d1
seungjoo <pool9604@naver.com>
2021-10-17 22:39:25 +0900

    learn namespace & vscode control

---
+++ problem1-4/problem1.h
@@ -0,0 +1,12 @@
+#ifndef __PROBLEM1_H_
+#define __PROBLEM1_H_
+    #include <iostream>
+    namespace BestComImpl
+    {
+     void SimpleFunc(void);
+    }
+    namespace ProgComImpl
+    {
+     void SimpleFunc(void);
+    }
+#endif 
---
+++ problem1-4/problem1_main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "problem1.h"
+int main()
+{
+    BestComImpl::SimpleFunc();
+    ProgComImpl::SimpleFunc();
+
+    return 0;
+}
